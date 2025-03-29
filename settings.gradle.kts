487fb9228e9aa912d48ecb94018568a8ac0e7f37
The Panda Oliver <Oliver.h.froberg@gmail.com>
2025-03-29 14:17:51 +0100

    Update stonecutter version and adjust build settings
    Upgraded stonecutter to version 1.21.4 and included a local build in the plugin management. Disabled Kikugie snapshot Maven repository for a cleaner configuration.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+	includeBuild("C:\\Users\\Olive\\IdeaProjects\\stonecutter")
 	repositories {
 		mavenCentral()
 		gradlePluginPortal()
@@ -6,7 +7,7 @@ pluginManagement {
 		maven("https://maven.architectury.dev")
 		maven("https://maven.minecraftforge.net")
 		maven("https://maven.neoforged.net/releases/")
-		maven("https://maven.kikugie.dev/snapshots")
+//		maven("https://maven.kikugie.dev/snapshots")
 	}
 }
 
