3770415705bd31d977b9cfc1e38ff9cdf9695f43
The Panda Oliver <Oliver.h.froberg@gmail.com>
2025-03-30 23:46:05 +0200

    Update mod to target Minecraft 1.21.4 and specify client dist
    Modified `stonecutter.gradle.kts` to activate version 1.21.4 and updated the `TemplateModNeoForgeClient` class to explicitly define the client distribution using `dist = Dist.CLIENT`. These changes ensure compatibility with the newer Minecraft version.

--- neoforge/src/main/java/com/example/template/neoforge/client/TemplateModNeoForgeClient.java
+++ neoforge/src/main/java/com/example/template/neoforge/client/TemplateModNeoForgeClient.java
@@ -2,9 +2,10 @@ package com.example.template.neoforge.client;
 
 import com.example.template.TemplateModCommon;
 import com.example.template.client.TemplateModCommonClient;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.Mod;
 
-@Mod(value = TemplateModCommon.MOD_ID /*? if >1.20.5 {*/ /*, dist = Dist.CLIENT *//*?}*/)
+@Mod(value = TemplateModCommon.MOD_ID /*? if >1.20.5 {*/ , dist = Dist.CLIENT /*?}*/)
 public class TemplateModNeoForgeClient {
 	public TemplateModNeoForgeClient() {
 		TemplateModCommonClient.init();
